9dee250d4b22f50a41b8d116307c85590da86ebf
Yabo-San <seniorpepex@gmail.com>
2018-07-17 00:30:02 -0700

    Revert "accidentally broke some stuff"
    This reverts commit fc9e19abc562acd906ca17f3c4a185e2411775ed.

--- Assets/scripts/SplashScreen.cs
+++ Assets/scripts/SplashScreen.cs
@@ -4,32 +4,19 @@ using UnityEngine;
 
 using UnityEngine.SceneManagement;
 
-public class SplashScreen : MonoBehaviour
-{
+public class SplashScreen : MonoBehaviour {
 
-    public string scene1;
-    public string scene2;
+    public string sceneToLoad;
     public int secTillScenesLoad;
 
-    // Use this for initialization
-    void Update()
-    {
-        //Invoke("OpenNextScene", secTillScenesLoad);
-        OpenNextScene();
+	// Use this for initialization
+	void Start () {
+        Invoke("OpenNextScene", secTillScenesLoad);
+	}
+	
+	// Update is called once per frame
+	void OpenNextScene () {
+        SceneManager.LoadScene(sceneToLoad);
 
-    }
-
-    // Update is called once per frame
-    void OpenNextScene()
-    {
-        if (Input.GetKeyDown("d"))
-        {
-            SceneManager.LoadScene(scene1);
-        }
-        else if (Input.GetAxisRaw("Horizontal") < 0)
-        {
-            SceneManager.LoadScene(scene2);
-        }
-
-    }
+	}
 }
